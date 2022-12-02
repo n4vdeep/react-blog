6826fb7c28d00e5fe03f0511c883d7a862596f77
Nav Dhamu <n4vdeep@gmail.com>
2022-12-02 18:53:10 +0000

    header and homepage basis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Home from "./pages/home/Home"
 import NavBar from './navbar/NavBar';
 
 function App() {
   return (
     <React.Fragment>    
       <NavBar />
+      <Home />
     </React.Fragment>
   );
 }
