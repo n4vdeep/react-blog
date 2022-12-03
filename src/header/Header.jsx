00759a7ed6b42944174eeb0515c2d39f9ef5efa2
Nav Dhamu <n4vdeep@gmail.com>
2022-12-03 11:02:09 +0000

    header complete

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,7 +3,11 @@ import './header.css'
 export default function Header() {
   return (
     <div className="header">
-      header
+      <div className="headerTitle">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img className="headerImg" src="https://www.superprof.co.uk/blog/wp-content/uploads/2018/02/landscape-photography-tutorials.jpg" alt="winding_road_image" />
     </div>
   )
 }
